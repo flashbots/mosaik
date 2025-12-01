6b514f91b87974a8000cd5d6f3dfc414c1859e4d
elizabeth <elizabethjbinks@gmail.com>
2025-12-01 16:36:30 -0500

    update tests for builder pattern

--- tests/pubsub.rs
+++ tests/pubsub.rs
@@ -17,18 +17,30 @@ struct Data3(pub String);
 async fn api_design_auto_disc() {
 	let network_id = NetworkId::random();
 
-	let n0 = Network::new(network_id.clone()).await.unwrap();
+	let n0 = NetworkBuilder::new(network_id.clone())
+		.build_and_run()
+		.await
+		.unwrap();
 	let mut p0 = n0.produce::<Data1>();
-	let p1 = n0.produce::<Data2>();
+	let _p1 = n0.produce::<Data2>();
 
-	let n1 = Network::new(network_id.clone()).await.unwrap();
-	let c1 = n1.consume::<Data1>();
-	let p1 = n1.produce::<Data3>();
+	// let n1 = Network::run(network_id.clone()).await.unwrap();
+	let n1 = NetworkBuilder::new(network_id.clone())
+		.with_bootstrap_peer(n0.local().addr())
+		.build_and_run()
+		.await
+		.unwrap();
+	let _c1 = n1.consume::<Data1>();
+	let _p1 = n1.produce::<Data3>();
 
-	let n2 = Network::new(network_id.clone()).await.unwrap();
+	let n2 = NetworkBuilder::new(network_id.clone())
+		.with_bootstrap_peer(n0.local().addr())
+		.build_and_run()
+		.await
+		.unwrap();
 	let mut c2a = n2.consume::<Data1>();
-	let c2b = n2.consume::<Data2>();
-	let c2c = n2.consume::<Data3>();
+	let _c2b = n2.consume::<Data2>();
+	let _c2c = n2.consume::<Data3>();
 
 	n1.discovery().dial(n0.local().addr()).await.unwrap();
 	n2.discovery().dial(n0.local().addr()).await.unwrap();
@@ -44,14 +56,20 @@ async fn api_design_auto_disc() {
 async fn api_design_manual_disc() -> anyhow::Result<()> {
 	let network_id = NetworkId::random();
 
-	let n0 = Network::new(network_id.clone()).await?;
+	let n0 = NetworkBuilder::new(network_id.clone())
+		.build_and_run()
+		.await?;
 	let mut p0_1 = n0.produce::<Data1>();
 
-	let n1 = Network::new(network_id.clone()).await?;
+	let n1 = NetworkBuilder::new(network_id.clone())
+		.build_and_run()
+		.await?;
 	let mut c1_1 = n1.consume::<Data1>();
 	let mut p1_1 = n1.produce::<Data1>();
 
-	let n2 = Network::new(network_id.clone()).await?;
+	let n2 = NetworkBuilder::new(network_id.clone())
+		.build_and_run()
+		.await?;
 	let mut c2_1 = n2.consume::<Data1>();
 
 	full_manual_disco(&[&n0, &n1, &n2]);
@@ -74,7 +92,9 @@ async fn api_design_manual_disc() -> anyhow::Result<()> {
 
 	assert_eq!(recv_c2, Some(Data1("Three".into())));
 
-	let n3 = Network::new(network_id.clone()).await?;
+	let n3 = NetworkBuilder::new(network_id.clone())
+		.build_and_run()
+		.await?;
 	let mut c3_1 = n3.produce::<Data1>();
 
 	full_manual_disco(&[&n1, &n2, &n3]);
